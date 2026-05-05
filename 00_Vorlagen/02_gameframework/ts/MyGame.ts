b485308e788adb2e8b3e3d3adb2d7ad6c80b59d3
andreas riedmann <andreas.riedmann@htldornbirn.at>
2026-05-05 11:07:10 +0200

    simplify game

--- 00_Vorlagen/02_gameframework/ts/MyGame.ts
+++ 00_Vorlagen/02_gameframework/ts/MyGame.ts
@@ -4,8 +4,7 @@ import { Game, GameFramework } from "./GameFramework.js";
 class MyGame extends Game {
   private x: number = 0;
   private y: number = 0;
-  private x1: number = 0;
-  private y1: number = 50;
+
   init(): void {
     console.log("Game started!");
   }
@@ -15,11 +14,6 @@ class MyGame extends Game {
     if (this.x > 800) {
       this.x = -100; // Reset position when it goes off screen
     }
-
-    this.y1 += 200 * deltaTime; // Move 100 pixels per second
-    if (this.y1 > 600) {
-      this.y1 = -100; // Reset position when it goes off screen
-    }
   }
 
   render(ctx: CanvasRenderingContext2D): void {
@@ -27,9 +21,6 @@ class MyGame extends Game {
 
     ctx.fillStyle = "#66aaff";
     ctx.fillRect(this.x, this.y, 100, 100);
-
-    ctx.fillStyle = "#66aaff";
-    ctx.fillRect(this.x1, this.y1, 100, 100);
   }
 }
 
